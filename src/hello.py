2e8a2617ae3bc68f06d6903357e8190a170ed5a0
Beatris-bit <145532987+Beatris-bit@users.noreply.github.com>
2026-06-09 16:34:21 +0200

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -15,7 +15,7 @@ def hello_spencer():
     f.close()
 
     return jsonify({
-        "message": "Hello Beatris",
+        "message": "Hello Sonne",
         "counter" : counter,
         "status": "success"
     })
